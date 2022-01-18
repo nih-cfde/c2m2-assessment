2a63ae524cc65ea3c649ca57b0519b2102612112
Daniel Clarke <u8sand@gmail.com>
2022-01-18 18:13:07 -0500

    Skip landing page check altogether, persistent_ids seem to be rarely resolvable

--- c2m2_assessment/rubric.py
+++ c2m2_assessment/rubric.py
@@ -893,39 +893,43 @@ def _(CFDE, full=False, **kwargs):
   'extended': True,
 })
 def _(CFDE, full=False, **kwargs):
-  import requests
-  results = {}
-  for file in CFDE.tables['file'].entities():
-    file_id = (file['id_namespace'], file['local_id'])
-    results[file_id] = {}
-    persistent_id = file.get('persistent_id')
-    if not persistent_id:
-      results[file_id]['value'] = 0
-      results[file_id]['comment'] = "No persistent id present"
-      continue
-    if not re.match(r'^https?://', persistent_id):
-      persistent_id = 'https://identifiers.org/{}'.format(persistent_id)
-    try:
-      status_code = requests.head(persistent_id, headers={'User-Agent': None}).status_code
-      results[file_id]['comment'] = f"Status Code: {status_code}"
-      if status_code >= 200 and status_code < 300:
-        results[file_id]['value'] = 1.0
-      elif status_code >= 300 and status_code < 399:
-        results[file_id]['value'] = 0.5
-      elif status_code >= 400:
-        results[file_id]['value'] = 0.25
-    except Exception as e:
-      results[file_id]['value'] = 0.0
-      results[file_id]['comment'] = f"Error: {e}"
-  results = pd.DataFrame(results).T
   return {
-    'value': results['value'].mean(),
-    'comment': f'based on status_code reports via HEAD',
-    'supplement': results.to_dict() if full else pd.concat([
-      results.head(5),
-      results.tail(5),
-    ]).to_dict(),
+    'value': float('nan'),
+    'comment': 'Skipped',
   }
+  # import requests
+  # results = {}
+  # for file in CFDE.tables['file'].entities():
+  #   file_id = (file['id_namespace'], file['local_id'])
+  #   results[file_id] = {}
+  #   persistent_id = file.get('persistent_id')
+  #   if not persistent_id:
+  #     results[file_id]['value'] = 0
+  #     results[file_id]['comment'] = "No persistent id present"
+  #     continue
+  #   if not re.match(r'^https?://', persistent_id):
+  #     persistent_id = 'https://identifiers.org/{}'.format(persistent_id)
+  #   try:
+  #     status_code = requests.head(persistent_id, headers={'User-Agent': None}).status_code
+  #     results[file_id]['comment'] = f"Status Code: {status_code}"
+  #     if status_code >= 200 and status_code < 300:
+  #       results[file_id]['value'] = 1.0
+  #     elif status_code >= 300 and status_code < 399:
+  #       results[file_id]['value'] = 0.5
+  #     elif status_code >= 400:
+  #       results[file_id]['value'] = 0.25
+  #   except Exception as e:
+  #     results[file_id]['value'] = 0.0
+  #     results[file_id]['comment'] = f"Error: {e}"
+  # results = pd.DataFrame(results).T
+  # return {
+  #   'value': results['value'].mean(),
+  #   'comment': f'based on status_code reports via HEAD',
+  #   'supplement': results.to_dict() if full else pd.concat([
+  #     results.head(5),
+  #     results.tail(5),
+  #   ]).to_dict(),
+  # }
 
 #%%
 @rubric.metric({
