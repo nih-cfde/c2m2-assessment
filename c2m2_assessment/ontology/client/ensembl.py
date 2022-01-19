216af18ec7941dcbd98dee3ec80f3a5d132551a2
Daniel Clarke <u8sand@gmail.com>
2022-01-19 14:54:40 -0500

    Fixup Ensembl ontology client

--- c2m2_assessment/ontology/client/ensembl.py
+++ c2m2_assessment/ontology/client/ensembl.py
@@ -4,6 +4,9 @@ import urllib.request, urllib.parse
 from c2m2_assessment.ontology.client.service import Service
 
 class EnsemblClient(Service):
+  def __init__(self):
+    super().__init__('ensembl')
+
   def _lookup(self, gene):
     T = yaml.safe_load(urllib.request.urlopen(
       f"https://rest.ensembl.org/lookup/id/{urllib.parse.quote(gene)}",
